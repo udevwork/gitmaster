bc44c9b268be00eb703645340df007996a4dd85b
udevwork@gmail.com <udevwork@gmail.com>
2020-03-19 19:55:48 +0300

    init commit

---
+++ testproject/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testproject
+//
+//  Created by Denis Kotelnikov on 10.03.2020.
+//  Copyright © 2020 Denis Kotelnikov. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ testproject/SceneDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  SceneDelegate.swift
+//  testproject
+//
+//  Created by Denis Kotelnikov on 10.03.2020.
+//  Copyright © 2020 Denis Kotelnikov. All rights reserved.
+//
+
+import UIKit
+import SwiftUI
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        let contentView = MainViewController()
+        if let windowScene = scene as? UIWindowScene {
+            let window = UIWindow(windowScene: windowScene)
+            window.rootViewController = contentView
+            
+            self.window = window
+            window.makeKeyAndVisible()
+        }
+    }
+}
+
+class MainViewController: UIViewController {
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        view.backgroundColor = .lightGray
+        
+      
+        
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
